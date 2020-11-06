e61b83b4d13af2c46c34ec17bb1101017dd8b33f
John Simonsen <johnsimonsen@weber.edu>
2020-11-06 10:28:44 -0700

    Update useless_trivia.py

--- lesson02/useless_trivia.py
+++ lesson02/useless_trivia.py
@@ -10,9 +10,8 @@ age = int(age)
 
 weight = int(input("Okay, last question.  How many pounds do you weigh? "))
 
-print("\nIf poet ee cummings were to email you, he'd address you as",
-      name.lower())
-print("But if ee were mad, he'd call you", name.upper())
+print(f"\nIf poet ee cummings were to email you, he'd address you as {name.lower()}")
+print(f"But if ee were mad, he'd call you {name.upper()}")
 
 called = name * 5
 print("\nIf a small child were trying to get your attention",)
@@ -26,5 +25,5 @@ moon_weight = weight / 6
 print(f"\nDid you know that on the moon you would weigh only {moon_weight} pounds?")
 
 sun_weight = weight * 27.1
-print(f"On the sun, you'd weigh {sun_weight}, (but, ah... not for long).")
+print(f"On the sun, you'd weigh {sun_weight} (but, ah... not for long).")
 
