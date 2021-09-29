290e9612109c56d24b46da1197a331e2dbbbdaa3
mrsimonsen <johnsimonsen@weber.edu>
2021-09-29 16:39:28 -0600

    rearranged code

--- lesson06/pad_it.py
+++ lesson06/pad_it.py
@@ -1,10 +1,10 @@
-print("Padding with leading 0s:")
-for i in range(1,11):
-    print(f"The number is {i:02}")
-
 from math import pi
 print("\nPadding decimal places:")
 print(f"The value of pi is {pi:.4f}")
+
+print("Padding with leading 0s:")
+for i in range(1,11):
+    print(f"The number is {i:02}")
 
 print("\nPadding with spaces:")
 for i in range(1,11):
