1861649e93f9319be3c5f97f683cbf3b5caf1202
mrsimonsen <johnsimonsen@weber.edu>
2022-09-06 11:15:38 -0600

    25 complete

--- 7-Files/25/pickle_it.py
+++ 7-Files/25/pickle_it.py
@@ -7,24 +7,22 @@ print("Pickling lists.")
 variety = ["sweet", "hot", "dill"]
 shape = ["whole", "spear", "chip"]
 brand = ["Claussen", "Heinz", "Vlassic"]
-f = open("pickles1.dat", "wb")
-pickle.dump(variety, f)
-pickle.dump(shape, f)
-pickle.dump(brand, f)
-f.close()
+with open("pickles1.dat", "wb") as file:
+	pickle.dump(variety, file)
+	pickle.dump(shape, file)
+	pickle.dump(brand, file)
 
 print("\nUnpickling lists.")
-f = open("pickles1.dat", "rb")
-variety = pickle.load(f)
-shape = pickle.load(f)
-brand = pickle.load(f)
+with open("pickles1.dat", "rb") as file:
+	variety = pickle.load(file)
+	shape = pickle.load(file)
+	brand = pickle.load(file)
 print(variety)
 print(shape)
 print(brand)
-f.close()
 
 print("\nShelving lists.")
-s = shelve.open("pickles2.dat")
+s = shelve.open("pickles2.bin")
 s["variety"] = ["sweet", "hot", "dill"]
 s["shape"] = ["whole", "spear", "chip"]
 s["brand"] = ["Claussen", "Heinz", "Vlassic"]
