c622637c5d11dd6da1259196912b68d4e7b9fb2b
mrsimonsen <johnsimonse@weber.edu>
2023-01-23 10:00:46 -0700

    fixed shuffleing cards

--- 4-Sequences,Exceptions/13/shuffle_cards.py
+++ 4-Sequences,Exceptions/13/shuffle_cards.py
@@ -2,9 +2,9 @@
 import random
 #create a deck of cards
 deck = []
-for suit in ('S','D','C','H'):
+for suit in ('S','C','D','H'):
 	#Spades, Clubs, Diamonds, Hearts
-	for value in ('A','1','2','3','4','5','6','7','8','9','10','J','Q','K'):
+	for value in ('A','2','3','4','5','6','7','8','9','10','J','Q','K'):
 		#Face values
 		deck += [value+suit]
 print(f"Original deck:\n{deck}")
