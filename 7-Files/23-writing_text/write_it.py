42e4252dd68852038d7fd8e7151d7cc3f3226102
mrsimonsen <johnsimonsen@weber.edu>
2023-04-12 10:02:00 -0600

    added notes about line endings and context managers

--- 7-Files/23-writing_text/write_it.py
+++ 7-Files/23-writing_text/write_it.py
@@ -1,11 +1,21 @@
 # Write It
 # Demonstrates writing to a text file
 
+#Line endings
+#Linux/MacOS newline (LF - line feed): \n
+#Windows newline (CRLF - carriage return, line feed): \r\n
+
+#with statement - context manager
 print("Creating a text file with the write() method.")
 with open("write_it.txt", "w") as file:
 	file.write("Line 1\n")
 	file.write("This is line 2\n")
 	file.write("That makes this line 3\n")
+
+#example without context manager
+#file = open("write_it.txt", "w")
+# do things with file
+#file.close()
 
 print("\nReading the newly created file.")
 with open("write_it.txt", "r") as file:
